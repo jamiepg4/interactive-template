b03ec9f7fc9f4542e06d22be8fc6cf82cc29f231
Kit Cross <kit.cross@fusion.net>
2015-06-01 15:11:04 -0400

    Switch to rsync for publishing operations

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var execSync = require('child_process').execSync;
 var p = require('../../package.json');
 
-gulp.task('clear-server', ['dist'], function() {
-    execSync('gsutil -m rm "' + p.publishUrl + '**"', true);
-});
-
-gulp.task('publish', ['clear-server'], function() {
-    execSync('gsutil -m cp -R ./dist/** ' + p.publishUrl, true);
+gulp.task('publish', ['dist'], function() {
+    execSync('gsutil -m rsync -r -d ./dist/ ' + p.publishUrl);
 });
